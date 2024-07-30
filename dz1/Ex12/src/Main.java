1da40885ce0e251dd45d0f54cbda9a4df3de0c48
white3091 <katena.navyshnaya.00@mail.ru>
2024-07-31 00:38:51 +0300

    ex12 for

--- dz1/Ex12/src/Main.java
+++ dz1/Ex12/src/Main.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+
+}import java.util.Scanner;
 public class Main { /*Перемножить числа от 1 до числа (включительно)
 введенного через аргумент к исполняемой программе*/
     public static void main(String[] args) {
