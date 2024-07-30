e23ede52dd3178495aecc93eeb59756f3e472995
white3091 <white309@yandex.ru>
2024-07-31 01:17:25 +0300

    22

---
+++ dz1/Ex22/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {  //каждый второй
+    public static void main(String[] args) {
+        String str1 = "первый";
+        String str2 = "второй";
+        String str3 = "третий";
+        String str4 = null;
+        String str5 = "пятый";
+        String[] strings = new String[]{str1,str2,str3,str4,str5};
+        int length = strings.length;
+        for (int i = 1; i <strings.length ; i=i+2) {
+            System.out.println(strings[i]);
+        }
+    }
+}
