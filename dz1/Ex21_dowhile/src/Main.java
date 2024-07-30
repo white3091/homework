392dc5789e619d4bb6bad62cb9bb890b1292a539
white3091 <white309@yandex.ru>
2024-07-31 01:00:00 +0300

    ex21 dowhile

--- dz1/Ex21_dowhile/src/Main.java
+++ dz1/Ex21_dowhile/src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
+public class Main {       // do while
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "первый";
+        String str2 = "второй";
+        String str3 = "третий";
+        String str4 = null;
+        String str5 = "пятый";
+        String[] strings = new String[]{str1,str2,str3,str4,str5};
+        int length = strings.length;
+        int i=0;
+        do{
+            System.out.println(strings[i]);
+            i++;
+        }
+        while (i<strings.length);
     }
 }
